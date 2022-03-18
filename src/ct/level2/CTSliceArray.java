f438d492b783516fb2f03ef25c0e64956ad20ea3
beomshin.kim <chzh859@naver.com>
2022-03-19 00:52:11 +0900

    #CTSliceArray

--- src/ct/level2/CTSliceArray.java
+++ src/ct/level2/CTSliceArray.java
@@ -1,23 +1,18 @@
 package ct.level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 // n^2 배열 자르기
 public class CTSliceArray {
 
     public static int[] solution(int n, long left, long right) {
-        int[] answer = {};
-        String s = "";
-        for(int i=0; i < n ; i++) {
-            for(int j=0 ; j < n ; j ++) {
-                long p = (n * i) + j;
-                if(p >= left && p <= right) {
-                    s += (Math.max(i, j) + 1) + "";
-                }
-            }
+        int[] answer = new int[(int)(right-left) + 1];
+        int idx=  0;
+        for(long i=left; i <= right ; i++) {
+            answer[idx++] = Math.max((int)i%n , (int)i/n) + 1;
         }
-
-        System.out.println(s);
 
         return answer;
     }
