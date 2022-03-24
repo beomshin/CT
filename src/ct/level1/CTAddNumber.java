bee71a27d6cfdc8cb92040e4d219f0cf03a0df90
beomshin.kim <chzh859@naver.com>
2022-03-25 00:58:52 +0900

    #없는 숫자 더하기

---
+++ src/ct/level1/CTAddNumber.java
@@ -0,0 +1,22 @@
+package ct.level1;
+
+import java.util.HashSet;
+
+// 없는 숫자 더하기
+public class CTAddNumber {
+
+    public int solution(int[] numbers) {
+        int answer = -1;
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n : numbers) {
+            set.add(n);
+        }
+
+        for(int i=1; i <10; i++) {
+            if(!set.contains(i)) answer += i;
+        }
+
+        return answer;
+    }
+}
